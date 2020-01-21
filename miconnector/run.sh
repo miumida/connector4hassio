ce64a3d4566cda69f5dbc034fcaaaca9202431a3
miumida <46196137+miumida@users.noreply.github.com>
2020-01-21 20:20:16 +0900

    Create run.sh

---
+++ miconnector/run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+CONFIG_PATH=/data/options.json
+
+echo lgtv2mqtt!
+
+DATA_PATH=$(jq --raw-output ".data_path" $CONFIG_PATH)
+MQTT_SERVER=$(jq --raw-output ".mqtt_server" $CONFIG_PATH)
+MQTT_USER=$(jq --raw-output ".mqtt_user" $CONFIG_PATH)
+LGWEBOSTV=$(jq --raw-output ".lgwebostv" $CONFIG_PATH)
+
+echo "[Info] MQTT Sever is [${MQTT_SERVER}] : USER( ${MQTT_USER} )"
+echo "[Info] LGWEBOSTV IP is [${LGWEBOSTV}"]
+
+# Check if config exists already
+mkdir -p $DATA_PATH
+
+if [[ -f $DATA_PATH/configuration.yaml ]]; then
+    if [[ ! -f $DATA_PATH/.configuration.yaml.bk ]]; then
+        echo "[Info] Configuration file found in data path, but no backup file found in data path. Backing up existing configuration to ${DATA_PATH}/.configuration.yaml.bk"
+        cp $DATA_PATH/configuration.yaml $DATA_PATH/.configuration.yaml.bk
+    else
+        echo "[Info] Configuration backup found in ${DATA_PATH}/.configuration.yaml.bk. Skipping config backup."
+    fi
+fi
+
+# Parse config
+cat "$CONFIG_PATH" | jq 'del(.data_path)' > $DATA_PATH/configuration.yaml
+
+# RUN lgtv2mqtt
+node /usr/lib/node_modules/lgtv2mqtt/index.js --verbosity=info --name=lgtv --url=$MQTT_SERVER --tv=$LGWEBOSTV
