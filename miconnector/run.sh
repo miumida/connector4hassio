16f2230c106398594f99b28f000a82b58cd2b772
miumida <46196137+miumida@users.noreply.github.com>
2020-01-27 23:34:43 +0900

    Update run.sh

--- miconnector/run.sh
+++ miconnector/run.sh
@@ -5,6 +5,9 @@ CONFIG_PATH=/data/options.json
 echo Mi-Connector!
 
 DATA_PATH=$(jq --raw-output ".data_path" $CONFIG_PATH)
+MQTT_SERVER=$(jq --raw-output ".mqtt_server" $CONFIG_PATH)
+MQTT_USER=$(jq --raw-output ".mqtt_user" $CONFIG_PATH)
+LGWEBOSTV=$(jq --raw-output ".lgwebostv" $CONFIG_PATH)
 
 echo "Mi-Connector start"
 
@@ -21,18 +24,18 @@ chmod a+x /usr/local/bin/dockerpull
 dockerpull /opt/miconnector fison67/mi-connector-arm:latest
 
 
-#if [[ -f $DATA_PATH/configuration.yaml ]]; then
-#    if [[ ! -f $DATA_PATH/.configuration.yaml.bk ]]; then
-#        echo "[Info] Configuration file found in data path, but no backup file found in data path. Backing up existing configuration to ${DATA_PATH}/.configuration.yaml.bk"
-#        cp $DATA_PATH/configuration.yaml $DATA_PATH/.configuration.yaml.bk
-#    else
-#        echo "[Info] Configuration backup found in ${DATA_PATH}/.configuration.yaml.bk. Skipping config backup."
-#    fi
-#fi
+if [[ -f $DATA_PATH/configuration.yaml ]]; then
+    if [[ ! -f $DATA_PATH/.configuration.yaml.bk ]]; then
+        echo "[Info] Configuration file found in data path, but no backup file found in data path. Backing up existing configuration to ${DATA_PATH}/.configuration.yaml.bk"
+        cp $DATA_PATH/configuration.yaml $DATA_PATH/.configuration.yaml.bk
+    else
+        echo "[Info] Configuration backup found in ${DATA_PATH}/.configuration.yaml.bk. Skipping config backup."
+    fi
+fi
 
 # Parse config
 cat "$CONFIG_PATH" | jq 'del(.data_path)' > $DATA_PATH/configuration.yaml
 
 
 # RUN lgtv2mqtt
-#node /usr/lib/node_modules/lgtv2mqtt/index.js --verbosity=info --name=lgtv --url=$MQTT_SERVER --tv=$LGWEBOSTV
+node /usr/lib/node_modules/lgtv2mqtt/index.js --verbosity=info --name=lgtv --url=$MQTT_SERVER --tv=$LGWEBOSTV
