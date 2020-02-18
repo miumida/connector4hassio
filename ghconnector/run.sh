61c28bdcdf664b91f75d5e8336c7d4539cc35337
miumida <46196137+miumida@users.noreply.github.com>
2020-02-18 23:22:29 +0900

    Update run.sh

--- ghconnector/run.sh
+++ ghconnector/run.sh
@@ -12,11 +12,11 @@ echo "[Info] ghconnector4hassio addons Start Ready"
 mkdir -p $DATA_PATH
 mkdir -p /config/ghconnector
 
-cp config.json /config/ghconnector/config.json
+cp gh-config.json /config/ghconnector/gh-config.json
 
 # Change config.js
 echo "[Info] ghconnector4hassio addons config.js editing"
-sed '4s|config/config.json|config/ghconnector/config.json|' util/config_bak.js > util/config.js
+sed '4s|config/gh-config.json|config/ghconnector/gh-config.json|' util/config_bak.js > util/config.js
 
 if [[ -f $DATA_PATH/configuration.yaml ]]; then
     if [[ ! -f $DATA_PATH/.configuration.yaml.bk ]]; then
