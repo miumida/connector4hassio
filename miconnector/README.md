62ece924ccaef7c07c4088c379a01968b8d940b6
miumida <46196137+miumida@users.noreply.github.com>
2020-02-25 21:20:16 +0900

    Add files via upload

---
+++ miconnector/README.md
@@ -0,0 +1,26 @@
+# Hass.io Add-on: Mi-Connector 
+
+![Supports armv7 Architecture][armv7-shield]
+
+### About
+아기나무집님이 만드신 mi-connector-arm 프로그램을 애드온으로 만든 것입니다.
+
+개발중입니다:'D
+
+### Version : 1.0.0(dev)
+
+### Reference Site
+- 아기나무집님의 mi_connector github (https://github.com/fison67/mi_connector)
+- dockerhub의 fison67/mi-connector-arm (https://hub.docker.com/r/fison67/mi-connector-arm)
+- DJJ PROJECT :: [IOT프로젝트] 2. mi-connector on AoL (https://blog.djjproject.com/608)<br>
+- DJJ PROJECT :: docker image rootfs 만들기 (https://blog.djjproject.com/626)<br>
+- android_over_linux : Configuration file repository (https://github.com/djjproject/android_over_linux)
+
+[forum]: https://cafe.naver.com/koreassistant
+[github]: https://github.com/HAKorea/addons
+[issue]: https://github.com/zooil/wallpadRS485/issues
+[aarch64-shield]: https://img.shields.io/badge/aarch64-yes-green.svg
+[amd64-shield]: https://img.shields.io/badge/amd64-yes-green.svg
+[armhf-shield]: https://img.shields.io/badge/armhf-yes-green.svg
+[armv7-shield]: https://img.shields.io/badge/armv7-yes-green.svg
+[i386-shield]: https://img.shields.io/badge/i386-yes-green.svg
---
+++ miconnector/run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+CONFIG_PATH=/data/options.json
+
+echo "[Info] Welcom to miconnector4hassio!"
+
+DATA_PATH=$(jq --raw-output ".data_path" $CONFIG_PATH)
+
+echo "[Info] miconnector4hassio addons Start Ready"
+
+# Check if config exists already
+mkdir -p $DATA_PATH
+mkdir -p /config/miconnector
+
+#cp config.json /config/miconnector/config.json
+
+# Change config.js
+echo "[Info] miconnector4hassio addons config.js editing"
+sed '4s|config/config.json|config/miconnector/config.json|' util/config_bak.js > util/config.js
+
+if [[ -f $DATA_PATH/configuration.yaml ]]; then
+    if [[ ! -f $DATA_PATH/.configuration.yaml.bk ]]; then
+        echo "[Info] Configuration file found in data path, but no backup file found in data path. Backing up existing configuration to ${DATA_PATH}/.configuration.yaml.bk"
+        cp $DATA_PATH/configuration.yaml $DATA_PATH/.configuration.yaml.bk
+    else
+        echo "[Info] Configuration backup found in ${DATA_PATH}/.configuration.yaml.bk. Skipping config backup."
+    fi
+fi
+
+# Parse config
+cat "$CONFIG_PATH" | jq 'del(.data_path)' > $DATA_PATH/configuration.yaml
+
+echo "[Info] Start miconnector:D"
+
+# start miconnector
+npm start
