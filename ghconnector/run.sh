d41fa7e6cfe05373965d80c9bcd35ce264859870
miumida <46196137+miumida@users.noreply.github.com>
2020-03-11 21:36:32 +0900

    Update run.sh

--- ghconnector/run.sh
+++ ghconnector/run.sh
@@ -14,20 +14,24 @@ mkdir -p /config/ghconnector
 
 # music1 volume directory create
 mkdir -p /share/ghconnector
-mkdir -p /share/ghconnecotr/music
+mkdir -p /share/ghconnector/music
 
 # Check gh-config.json file Not exists
 if [[ ! -f /config/ghconnector/gh-config.json ]]; then
     cp gh-config.json /config/ghconnector/gh-config.json
 fi
 
-# Change config.js
+# Change util/config.js
 echo "[Info] ghconnector4hassio addons config.js editing"
 sed '10,22s|config/gh-config.json|config/ghconnector/gh-config.json|' util/config_bak.js > util/config.js
 
-# Change log.js
+# Change util/log.js
 echo "[Info] ghconnector4hassio addons log.js editing"
 sed '30s|config/gh-connector.log|config/ghconnector/gh-connector.log|' util/log_bak.js > util/log.js
+
+# Change sevice/mp3.js
+echo "[Info] ghconnector4hassio addons log.js editing"
+sed '17s|/music1|/share/ghconnector/music|' service/mp3_bak.js > service/mp3.js
 
 # Check
 if [[ -f $DATA_PATH/configuration.yaml ]]; then
