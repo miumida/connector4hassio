bd381823901d623738687b8b8bb58ed2d251f1a5
miumida <46196137+miumida@users.noreply.github.com>
2021-03-08 22:08:04 +0900

    Update run.sh

--- lg-connector/run.sh
+++ lg-connector/run.sh
@@ -2,6 +2,11 @@
 mkdir -p /config/lg-connector
 mkdir -p /share/lg-connector
 
+# Check lg-config.json file Not exists
+if [[ ! -f /config/lg-connector/lg-config.json ]]; then
+    cp lg-config.json /config/lg-connector/lg-config.json
+fi
+
 # util/config.js
 echo "[Info] LG-Connector addons config.js editing"
 sed -i '10,23s|config/lg-config.json|config/lg-connector/lg-config.json|' util/config.js
