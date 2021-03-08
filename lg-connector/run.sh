2a2b9789fa46bd53c1d96afbbedfea92e3c5d793
miumida <46196137+miumida@users.noreply.github.com>
2021-03-08 22:06:48 +0900

    Update run.sh

--- lg-connector/run.sh
+++ lg-connector/run.sh
@@ -2,8 +2,12 @@
 mkdir -p /config/lg-connector
 mkdir -p /share/lg-connector
 
+# util/config.js
+echo "[Info] LG-Connector addons config.js editing"
+sed -i '10,23s|config/lg-config.json|config/lg-connector/lg-config.json|' util/config.js
+
 # Edit util/log.js
-echo "[Info] GH-Connector addons log.js editing"
+echo "[Info] LG-Connector addons log.js editing"
 sed -i '29s|config/lg-connector.log|config/lg-connector/lg-connector.log|' util/log.js
 
 npm start
