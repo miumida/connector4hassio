d0d2db829fcc761874ea0a4c9b048f619d8578ce
miumida <46196137+miumida@users.noreply.github.com>
2021-04-06 19:13:07 +0900

    Update run.sh

--- gh-connector/run.sh
+++ gh-connector/run.sh
@@ -16,6 +16,8 @@ sed -i '10,22s|config/gh-config.json|config/gh-connector/gh-config.json|' util/c
 echo "[Info] GH-Connector addons log.js editing"
 sed -i '30s|config/gh-connector.log|config/gh-connector/gh-connector.log|' util/log.js
 
+sed -i '337s|   console.log(req);' routes/googleHome.js
+
 # Change sevice/mp3.js
 echo "[Info] GH-Connector addons mp3.js editing"
 sed -i '17s|/music1|/share/gh-connector/music|' service/mp3.js
