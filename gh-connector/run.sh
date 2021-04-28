3d48ccdb70c3b80009cf598b9c4338b2c164a584
miumida <46196137+miumida@users.noreply.github.com>
2021-04-28 20:15:15 +0900

    Update run.sh

--- gh-connector/run.sh
+++ gh-connector/run.sh
@@ -1,7 +1,15 @@
+CONFIG_PATH=/data/options.json
+
+MUSIC1=$(jq --raw-output ".music1_path" $CONFIG_PATH)
+MUSIC2=$(jq --raw-output ".music2_path" $CONFIG_PATH)
+MUSIC3=$(jq --raw-output ".music3_path" $CONFIG_PATH)
+
 # Check if config exists already
 mkdir -p /config/gh-connector
 mkdir -p /share/gh-connector
 mkdir -p /share/gh-connector/music
+mkdir -p $MUSIC2
+mkdir -p $MUSIC3
 
 # Check gh-config.json file Not exists
 if [[ ! -f /config/gh-connector/gh-config.json ]]; then
